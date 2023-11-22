28902c2bf884668068f4510a6a435a57ea38835e
Carvas <76731842+lCarvas@users.noreply.github.com>
2023-11-22 17:04:36 +0000

    Update Trigger 1.sql

--- Triggers/Trigger 1.sql
+++ Triggers/Trigger 1.sql
@@ -14,7 +14,10 @@ BEGIN
                     left join [MATCH] ma on p.IDCOUNTRY = ma.IDAWAYTEAM
                    
 
-    WHERE p.IDCOUNTRY = m.IDHOMETEAM or p.IDCOUNTRY = ma.IDAWAYTEAM
+    WHERE p.IDCOUNTRY = m.IDHOMETEAM or p.IDCOUNTRY = ma.IDAWAYTEAM and m.IDMATCH = i.IDMATCH
+
+    if @@ROWCOUNT < (select count(*) from inserted)
+        print('There were records not being inserted because the player does not belong to any of the countries playing on the match.')
 
 end;
 go
